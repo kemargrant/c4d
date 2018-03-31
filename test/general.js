525398204dcf4e7ab9edeb226b481f5385cc7b5e
kemargrant <kemargrant@gmail.com>
2018-03-31 19:32:42 -0400

    Shorted nice order chain timeout

--- test/general.js
+++ test/general.js
@@ -36,8 +36,8 @@ describe('General Functions', function() {
 	});
 	
 	describe('#Save to Database', function() {
-		this.timeout(2000);
 		it('Should save and delete record from the database', function(done) {
+			this.timeout(2000);
 			var date = new Date().getTime();
 			return setTimeout( async function(){
 				var x = await bot.saveDB("trade",{"UUID":date});
@@ -61,8 +61,13 @@ describe('General Functions', function() {
 	});	
 	
 	describe('#NiceOrderChain', function() {
-		this.timeout(9901)
 		it('Should slow chain a series of functions', function(done) {
+			this.timeout(6010)
+			var bot = new CryptoBot.bot(mock.mockSettings1);
+			bot.https = mock.https;
+			bot.MongoClient = mock.MongoClient;
+			bot.DB = bot.database();
+			bot.DB = bot.database();
 			function sum(obj){
 				var count = 0;
 				for(var key in obj){
@@ -71,9 +76,7 @@ describe('General Functions', function() {
 				return count;
 			}
 			function cb(){console.log("nice order chain cb")}
-			bot.add = function(x,y){
-				return new Promise((resolve,reject)=>resolve(x+y))
-			}
+			bot.add = function(x,y){return new Promise((resolve,reject)=>resolve(x+y))}
 			var answer = {}
 			bot.niceOrderChain([bot.add,bot.add,bot.add,cb],answer).chain([[1,1],[1,1],[1,1]]).then(()=>{
 				assert.equal(sum(answer),6);
