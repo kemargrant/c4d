3aa0dd59d2f1486f7277d879432502d4aabf7406
kemargrant <kemargrant@gmail.com>
2018-03-31 19:15:41 -0400

    Test all branches of binanceCheckConditions

--- test/binance.js
+++ test/binance.js
@@ -5,85 +5,228 @@ var assert = require('assert');
 var WebSocket = require('ws');
 
 describe('#Arbitrage', function() {
+
+	describe('##Illiquid Trade', function() {	
+		var testBot = new CryptoBot.bot(mock.mockSettings1);
+		var base = 'ltcbtc';
+		testBot.MongoClient = mock.MongoClient;
+		testBot.DB = testBot.database();
+		testBot.https = mock.https;
+		testBot.email = mock.email;
+		testBot.binancePrec[base] = [6,2,2,2,6,5];
+		var pairs = ['ltcbtc','btcusdt','ltcusdt'];
+		var e1 = {'ltcbtc':'ltc'} 
+		var b1 = {'ltcbtc':'btc'} 
+		var u1 = {'ltcbtc':'usdt'} 
+		it('Should return false (Illiquid Trade)',function() {
+			testBot.binanceStrategy[base] = {
+			  one:{
+				 b: 8836,
+			     b_amount: 0,
+			     c: 18.38,
+			     c_amount: 0,
+			     a: 0.002077,
+			     a_amount: 416.84 },
+			  two: 
+			   { b: 8841,
+			     b_amount: 0.500171,
+			     c: 18.379,
+			     c_amount: 20.146,
+			     a: 0.002073,
+			     a_amount: 282 } 
+			}
+			testBot.liquidTradesBinance[base] = true;
+			testBot.binanceLimits[base] = {over:{lowerLimit:100,upperLimit:104},under:{lowerLimit:99,upperLimit:99.9}}
+			testBot.binanceBalance = {'bnb':1,'ltc':50,'btc':0.5,'usdt':4000}
+			var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
+			assert.equal(false,val)
+		});
+	})
+	describe('##Minimum b1 not satisfied', function() {	
 	var testBot = new CryptoBot.bot(mock.mockSettings1);
 	var base = 'ltcbtc';
 	testBot.MongoClient = mock.MongoClient;
 	testBot.DB = testBot.database();
 	testBot.https = mock.https;
 	testBot.email = mock.email;
-	testBot.binancePrec = {}
 	testBot.binancePrec[base] = [6,2,2,2,6,5];
 	var pairs = ['ltcbtc','btcusdt','ltcusdt'];
 	var e1 = {'ltcbtc':'ltc'} 
 	var b1 = {'ltcbtc':'btc'} 
 	var u1 = {'ltcbtc':'usdt'} 
-	it('Should return true',function() {
-		testBot.binanceStrategy[base] = {
-		  one:{
-			 b: 8836,
-		     b_amount: 0,
-		     c: 18.38,
-		     c_amount: 0,
-		     a: 0.002077,
-		     a_amount: 416.84 },
-		  two: 
-		   { b: 8841,
-		     b_amount: 0.500171,
-		     c: 18.379,
-		     c_amount: 20.146,
-		     a: 0.002073,
-		     a_amount: 282 } 
-		}
-		testBot.binanceLimits = {}
+		it('Should return false',function() {
+			testBot.binanceStrategy[base] = {
+			  one:{
+				 b: 8836,
+			     b_amount: 10,
+			     c: 18.38,
+			     c_amount: 0,
+			     a: 0.002077,
+			     a_amount: 416.84 },
+			  two: 
+			   { b: 8841,
+			     b_amount: 0.500171,
+			     c: 18.379,
+			     c_amount: 20.146,
+			     a: 0.002073,
+			     a_amount: 282 } 
+			}
+			testBot.liquidTradesBinance[base] = true;
+			testBot.binanceB1Min[base] = 2
+			testBot.binanceLimits[base] = {over:{lowerLimit:100,upperLimit:104},under:{lowerLimit:99,upperLimit:99.9}}
+			testBot.binanceBalance = {'bnb':1,'ltc':50,'btc':0.5,'usdt':4000}
+			var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
+			assert.equal(false,val)
+		});
+	})
+	describe('##Optimal Trade ? (<100)', function() {	
+	var testBot = new CryptoBot.bot(mock.mockSettings1);
+	var base = 'ltcbtc';
+	testBot.MongoClient = mock.MongoClient;
+	testBot.DB = testBot.database();
+	testBot.https = mock.https;
+	testBot.email = mock.email;
+	testBot.binancePrec[base] = [6,2,2,3,6,5];
+	var pairs = ['ltcbtc','btcusdt','ltcusdt'];
+	var e1 = {'ltcbtc':'ltc'} 
+	var b1 = {'ltcbtc':'btc'} 
+	var u1 = {'ltcbtc':'usdt'} 
+		it('Should return false',function() {
+			testBot.binanceStrategy[base] = {
+			  one:{
+				 b: 10741.05,
+			     b_amount: 10,
+			     c: 886.23,
+			     c_amount: 0,
+			     a: 0.082314,
+			     a_amount: 416.84 },
+			  two: 
+			   { b: 8841,
+			     b_amount: 0.500171,
+			     c: 18.379,
+			     c_amount: 20.146,
+			     a: 0.002073,
+			     a_amount: 282 } 
+			}
+			testBot.binanceOptimalTrades[base] = true;
+			testBot.binanceLimits[base] = {over:{lowerLimit:100,upperLimit:104},under:{lowerLimit:80,upperLimit:99.9}}
+			testBot.binanceBalance = {'bnb':1,'ltc':50,'btc':0.5,'usdt':4000}
+			var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
+			assert.equal(false,val)
+		});
+	})
+
+	describe('##Minimum u1 not satisfied', function() {	
+		var testBot = new CryptoBot.bot(mock.mockSettings1);
+		var base = 'ltcbtc';
+		testBot.MongoClient = mock.MongoClient;
+		testBot.DB = testBot.database();
+		testBot.https = mock.https;
+		testBot.email = mock.email;
+		testBot.binancePrec[base] = [6,2,2,2,6,5];
+		var pairs = ['ltcbtc','btcusdt','ltcusdt'];
+		var e1 = {'ltcbtc':'ltc'} 
+		var b1 = {'ltcbtc':'btc'} 
+		var u1 = {'ltcbtc':'usdt'} 	
+		testBot.liquidTradesBinance[base] = true;
+		testBot.binanceOptimalTrades[base] = true;
 		testBot.binanceLimits[base] = {over:{lowerLimit:100,upperLimit:104},under:{lowerLimit:99,upperLimit:99.9}}
-		testBot.binanceBalance = {'bnb':1,'ltc':50,'btc':0.5,'usdt':4000}
-		var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
-		assert.equal(true,val)
-	});
-	it('Should perform a trade and return true',function() {
-		testBot.binanceStrategy[base] = { 
-		one:{ 
-		     b: 8854,
-		     b_amount: 0.02048,
-		     c: 18.311,
-		     c_amount: 25,
-		     a: 0.00208,
-		     a_amount: 277.84 },
-		two: 
-		   { b: 8843.99,
-		     b_amount: 0.09,
-		     c: 18.321,
-		     c_amount: 4.102,
-		     a: 0.002072,
-		     a_amount: 66.05 } 
-		}
-		var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
-		assert.equal(val,true)
+		it('Should return false',function() {
+			testBot.binanceStrategy[base] = { 
+			one:{ 
+			     b: 8854,
+			     b_amount: 0.02048,
+			     c: 18.1,
+			     c_amount: 25,
+			     a: 0.00208,
+			     a_amount: 277.84 },
+			two: 
+			   { b: 8843.99,
+			     b_amount: 0.09,
+			     c: 18.321,
+			     c_amount: 4.102,
+			     a: 0.002072,
+			     a_amount: 66.05 } 
+			}
+			testBot.binanceU1Min[base] = 200;
+			var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
+			assert.equal(val,false)
+		});	
+	})
+		
+	describe('##Low Balance', function() {	
+		var testBot = new CryptoBot.bot(mock.mockSettings1);
+		var base = 'ltcbtc';
+		testBot.MongoClient = mock.MongoClient;
+		testBot.DB = testBot.database();
+		testBot.https = mock.https;
+		testBot.email = mock.email;
+		testBot.binancePrec[base] = [6,2,2,2,6,5];
+		var pairs = ['ltcbtc','btcusdt','ltcusdt'];
+		var e1 = {'ltcbtc':'ltc'} 
+		var b1 = {'ltcbtc':'btc'} 
+		var u1 = {'ltcbtc':'usdt'} 		
+		testBot.liquidTradesBinance[base] = true;
+		testBot.binanceLimits[base] = {over:{lowerLimit:100,upperLimit:104},under:{lowerLimit:99,upperLimit:99.9}}	
+		it('Should return false (wallet balance low)',function() {
+			testBot.binanceBalance.btc = 0;
+			testBot.binanceStrategy[base] = { 
+			one:{ 
+			     b: 8854,
+			     b_amount: 0.02048,
+			     c: 18.311,
+			     c_amount: 25,
+			     a: 0.00208,
+			     a_amount: 277.84 },
+			two: 
+			   { b: 8843.99,
+			     b_amount: 0.09,
+			     c: 18.321,
+			     c_amount: 4.102,
+			     a: 0.002072,
+			     a_amount: 66.05 } 
+			}
+			var val = testBot.binanceArbitrage(base,pairs,e1,b1,u1);
+			assert.equal(val,false)
 	});		
-	it('Should thow a promise reject Binance Arbitrage',async function() {
-		testBot.binanceStrategy[base] = { 
-		one:{ 
-		     b: 8854,
-		     b_amount: 0.02048,
-		     c: 18.311,
-		     c_amount: 25,
-		     a: 0.00208,
-		     a_amount: 277.84 },
-		two: 
-		   { b: 8843.99,
-		     b_amount: 0.09,
-		     c: 18.321,
-		     c_amount: 4.102,
-		     a: 0.002072,
-		     a_amount: 66.05 } 
-		}
-		var generic = function(){return new Promise((resolve,reject)=>reject(false))}
-		var val = await testBot.binanceBeginArbitrage('ltcbtc',99,"generic message", { btc: 0.004889, usdt: 43.23826711, ltc: 2.36004 }, { ltcbtc: 'ltc' }, { ltcbtc: 'btc' }, { ltcbtc: 'usdt' }, [generic()]);
-		assert.equal(val,false)
-	});			
-	
+	})
+	describe('##Arbitrage Error', function() {	
+		var testBot = new CryptoBot.bot(mock.mockSettings1);
+		var base = 'ltcbtc';
+		testBot.MongoClient = mock.MongoClient;
+		testBot.DB = testBot.database();
+		testBot.https = mock.https;
+		testBot.email = mock.email;
+		testBot.binancePrec[base] = [6,2,2,2,6,5];
+		var pairs = ['ltcbtc','btcusdt','ltcusdt'];
+		var e1 = {'ltcbtc':'ltc'} 
+		var b1 = {'ltcbtc':'btc'} 
+		var u1 = {'ltcbtc':'usdt'} 	
+		it('Should thow a promise reject Binance Arbitrage',async function() {
+			testBot.binanceStrategy[base] = { 
+			one:{ 
+			     b: 8854,
+			     b_amount: 0.02048,
+			     c: 18.311,
+			     c_amount: 25,
+			     a: 0.00208,
+			     a_amount: 277.84 },
+			two: 
+			   { b: 8843.99,
+			     b_amount: 0.09,
+			     c: 18.321,
+			     c_amount: 4.102,
+			     a: 0.002072,
+			     a_amount: 66.05 } 
+			}
+			var generic = function(){return new Promise((resolve,reject)=>reject(false))}
+			var val = await testBot.binanceBeginArbitrage('ltcbtc',99,"generic message", { btc: 0.004889, usdt: 43.23826711, ltc: 2.36004 }, { ltcbtc: 'ltc' }, { ltcbtc: 'btc' }, { ltcbtc: 'usdt' }, [generic()]);
+			assert.equal(val,false)
+		});			
+	})
 	describe('#Format Message < 100%', function() {
 		it('Should return a correctly formated string',function() {
+			var testBot = new CryptoBot.bot(mock.mockSettings1);
 			var format = "99.850% "+new Date().toString().split('GMT')[0]+"\n34.1ltc => 626.758 usdt @18.38\n626.755152usdt => 0.070932 btc @8836\n0.07082570btc => 34.15 ltc @0.002077\n"
 			var message = testBot.binanceArbitrageMessageFormat({ ltc: 34.1, usdt: 626.758, btc: 0.070932 },'ltc','usdt',18.38,'btc',8836,34.1,0.002077,2,99.84968443960827)
 			assert.equal(format,message)
@@ -91,11 +234,13 @@ describe('#Arbitrage', function() {
 	})
 	describe('#Format Message > 100%', function() {
 		it('Should return a correctly formated string',function() {
+			var testBot = new CryptoBot.bot(mock.mockSettings1);
 			var format = "100.020% "+new Date().toString().split('GMT')[0]+"\n0.004889btc => 43.23826711 usdt @8843.99\n43.23829284000001usdt => 2.36004 ltc @18.321\n2.36ltc => 0.004890 btc @0.002072\n"
 			var message = testBot.binanceArbitrageMessageFormat({ btc: 0.004889, usdt: 43.23826711, ltc: 2.36004 },'btc','usdt',8843.99,'ltc',18.321,2,0.002072,6,100.02045346869713)
 			assert.equal(format,message);
 		});		
 	})
+
 });	
 	
 
@@ -112,7 +257,6 @@ describe('Binance', function() {
 			var val = binanceBot.binanceUserStream('randomekey');
 			setTimeout(()=>{
 				assert(binanceBot.binanceUserStreamString.search(val.url.host) > -1);
-				//userStream.close();
 			},800);
 			
 		});
