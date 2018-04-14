dcb026d383aafbc2e7885b77ca26aff13eb01229
kemargrant <kemargrant@gmail.com>
2018-04-14 14:17:55 -0400

    Add additional binanceListenUser test

--- test/binance.js
+++ test/binance.js
@@ -519,6 +519,16 @@ describe('Binance', function() {
 		
 	});	
 
+	describe('#ListenUser', function() {
+		return it('Should return an Error', function() {
+			var binanceBot = new CryptoBot.bot(mock.mockSettings1);
+			binanceBot.binanceListenKey = function(){
+				return new Promise((resolve,reject)=>reject("Error"))
+			}
+			return binanceBot.binanceListenUser().catch(e=>assert.equal(e,"Error"));
+		});
+	})
+
 	describe('#Keep Alive', function() {
 		return it('Should return an empty object', async function() {
 			var val = await binanceBot.binanceListenKey();
